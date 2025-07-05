25d3e1d73733800194b144c0304cb7acae8c5aef
Seoyeong Ji <113742925+skyparkjsy@users.noreply.github.com>
2025-07-05 15:42:06 +0900

    [Silver III] Title: N과 M (2), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.cc
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.cc
@@ -1,36 +1,24 @@
 #include <iostream>
-
-// b15650. N과 M (2) - 백트래킹 - 가능한 모든 경우 
 #if 01
 using namespace std;
 
+int arr[8 + 1];
 int N, M;
-int cnt;
-int arr[9];
-void dfs(int n, int num, int valid) {
+void dfs(int n, int start) { // 고른 개수 n, 시작 start
 	if (n == M) {
-		if (valid == 1) {
-			for (int j = 0; j < M; ++j) {
-				cout << arr[j] << ' ';
-			}
-			cout << '\n';
+		for (int i = 0; i < M; ++i) {
+			cout << arr[i] << ' ';
 		}
+		cout << '\n';
 		return;
 	}
-	for (int i = 1; i <= N; ++i) {
-		if (valid == 1 && i > num) {
-			arr[n] = i;
-			dfs(n + 1, i, 1);
-		}
-		else {
-			dfs(n + 1, i, 0);
-		}
+	for (int i = start; i <= N; ++i) {
+		arr[n] = i;
+		dfs(n + 1, i + 1);
 	}
 }
 int main(void) {
 	cin >> N >> M;
-	dfs(0, 0, 1);
-	return 0;
+	dfs(0, 1);
 }
-
 #endif
