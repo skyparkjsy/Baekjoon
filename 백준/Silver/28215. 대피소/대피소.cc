80f681e871ec653d2148c68b44e56e544d63ede4
Seoyeong Ji <113742925+skyparkjsy@users.noreply.github.com>
2025-07-06 16:36:40 +0900

    [Silver IV] Title: 대피소, Time: 12 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/28215. 대피소/대피소.cc
+++ 백준/Silver/28215. 대피소/대피소.cc
@@ -15,15 +15,23 @@ int min_dist; // 집과 대피소 거리
 int max_val; // 최단거리들 중 최대
 int ans = INT_MAX;
 
+int is_shelter(int i) {
+	// 대피소인지 검사
+	for (int j = 0; j < K; ++j) {
+		if ((house[i].x == shelter[j].x) && (house[i].y == shelter[j].y)) return 1;
+	}
+	return 0;
+}
+
 void dfs(int n, int start) { // n은 선택한 대피소 수
 	if (n == K) {
-		max_val = INT_MIN;
+		max_val = 0;
 		// 모든 집에 대해, 가장 가까운 대피소 좌표
 		for (int i = 0; i < N; ++i) { 
+			if (is_shelter(i)) continue; // 이 집이 대피소면 continue
 			min_dist = INT_MAX;
 			// 대피소들 중 최단거리
 			for (int j = 0; j < K; ++j) { 
-				if (house[i].x == shelter[j].x && house[i].y == shelter[j].x) continue;
 				min_dist = min(min_dist, abs(house[i].x - shelter[j].x) + abs(house[i].y - shelter[j].y)); 
 			}
 			// 최단거리들 중 최대거리
@@ -34,9 +42,7 @@ void dfs(int n, int start) { // n은 선택한 대피소 수
 	}
 	for (int i = start; i < N; ++i) {
 		// 대피소 좌표 설정 (house[i]를 대피소로)
-		shelter[n].x = house[i].x;
-		shelter[n].y = house[i].y;
-
+		shelter[n] = house[i];
 		// 다음 대피소 선택 (i+1번째부터)
 		dfs(n + 1, i + 1);
 	}
